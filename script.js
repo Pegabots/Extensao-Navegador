f617d13166149a3999cb672ce52e6de7e5bc6b0c
heitorboccatousp <81279129+heitorboccatousp@users.noreply.github.com>
2022-09-03 13:46:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+replaceText(document.body)
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText)
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if (element.textContent.match(/Red Bull/gi)) {
+     
+     element.parentElement.remove();
+    }
+  }
+}
