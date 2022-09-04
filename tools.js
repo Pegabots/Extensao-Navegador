8b0176a79a18780f6df9320645ffd56dac5a339d
Lucas Cardoso <lucas.cardoso.santos@usp.br>
2022-09-04 03:00:28 -0300

    tools: create tools file
    Tools file will have all useful functions that can be shared to another
    files. Since services can't be modules and therefore can't import
    exported functions, this will not have the export keyword also.
    
    To import its functions, it will be needed to use the importScripts
    function.
    
    Signed-off-by: Lucas Cardoso <lucas.cardoso.santos@usp.br>

---
+++ tools.js
@@ -0,0 +1,19 @@
+/**
+ * Performs an API get to the given URL
+ * Deals with possible errors and converts the result
+ * 
+ * @param url get request path 
+ * @returns response as an javascript object
+ */
+async function apiGet(url) {
+  const pegaBotRequest = new Request(url);
+  
+  const response = await fetch(pegaBotRequest)
+  
+  if (!response.ok) {
+    throw new Error('HTTP Error!');
+  }
+
+  return await response.json();
+}
+  
